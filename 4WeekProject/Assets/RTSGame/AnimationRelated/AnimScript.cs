2d4eaaec2e3a8c8d27209c7c41bca28074bf5d67
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-11-09 14:38:38 +0100

    Added animation script
    This is currently very shit and it's on fire, but the animations are there and they work perfectly fine, it's just that the script I made is bad and the animator itself is not structured in a good manner

---
+++ 4WeekProject/Assets/RTSGame/AnimationRelated/AnimScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AnimScript : MonoBehaviour
+{
+    public bool activeOnAwake;
+    public bool loop;
+    public Animation anim;
+    public AnimationClip animClip;
+    public Animator animator;
+    private string animName;
+
+    private void Start()
+    {
+        animName = anim.clip.ToString();
+        if (loop)
+        {
+            animator.SetBool("loop", true);
+        }
+        if (activeOnAwake)
+        {
+            animator.SetBool("onAwake", true);
+        }
+        else
+        {
+            animator.SetBool("onAwake", false);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+            StartCoroutine(PlayAnimationOnce());
+        }
+    }
+    public void PlayAnimation(string Name)
+    {
+        animator.SetBool("active", true);
+        animator.Play(Name, 0);
+    }
+    public IEnumerator PlayAnimationOnce()
+    {
+        animator.SetBool("active", true);
+        animator.Play("idle", 0);
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("active", false);
+    }
+}
